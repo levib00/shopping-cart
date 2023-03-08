9c7407e4266553dbc06b651cc1ac2fc49ec31434
levib00 <levib8@hotmail.com>
2023-03-07 21:37:49 -0500

    Add tests for typed values

--- src/tests/shop-item.test.js
+++ src/tests/shop-item.test.js
@@ -1,3 +1,4 @@
+/* eslint-disable testing-library/no-unnecessary-act */
 import React from "react";
 import { getByRole, render, screen, act } from "@testing-library/react";
 import "@testing-library/jest-dom";
@@ -31,14 +32,12 @@ describe('Test quantity input', () => {
   it('test decrement button', () => {
     render(<ShopItem />)
     const incrementBtn = screen.getByRole('button', {name: '+'})
-    // eslint-disable-next-line testing-library/no-unnecessary-act
     act(() => {
       for (let i = 0; i < 3; i++) {
         userEvent.click(incrementBtn)
       }
     });
     const decrementBtn = screen.getByRole('button', {name: '-'})
-    // eslint-disable-next-line testing-library/no-unnecessary-act
     act(() => {
       for (let i = 0; i < 3; i++) {
         userEvent.click(decrementBtn)
@@ -72,3 +71,32 @@ describe('Test quantity input', () => {
     expect(quantity).toBe('25')
   })
 })
+
+describe("Typed values", () => {
+  it('max quantity works with typed values', () => {
+    render (<ShopItem />)
+    const quantity = screen.getByRole('textbox', {name:'quantity:'})
+    act(() => {
+      userEvent.type(quantity, '30')
+    });
+    expect(quantity.value).toBe('25')
+  });
+
+  it('min quantity works with typed values', () => {
+    render (<ShopItem />)
+    const quantity = screen.getByRole('textbox', {name:'quantity:'})
+    act(() => {
+        userEvent.type(quantity, '-30')
+    });
+    expect(parseInt(quantity.value)).toBeGreaterThan(-1)
+  });
+
+  it('input will not accept non-numbers', () => {
+    render (<ShopItem />)
+    const quantity = screen.getByRole('textbox', {name:'quantity:'})
+    act(() => {
+      userEvent.type(quantity, 'w];,-=+_9')
+    });
+    expect(quantity.value).toBe('9')
+  });
+});
