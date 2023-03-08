846525d39dcdd8f493b215accc5d3720f7833124
levib00 <levib8@hotmail.com>
2023-03-07 21:48:11 -0500

    Add bare bones home page.

---
+++ src/components/main.js
@@ -0,0 +1,11 @@
+import React from "react"
+
+export const Main = () => {
+  return (
+    <div> {/*will make this an image bg*/}
+      <h2>this is optional* maybe a tagline</h2>
+      <p>This is some babel about how great our newest product is.</p>
+      <button>SHOP NOW</button>
+    </div>
+  )
+}
