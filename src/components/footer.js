59c3cea4125551263d8d8302910ebc159de1f69e
levib00 <levib8@hotmail.com>
2023-03-07 21:47:37 -0500

    Add basic footer

---
+++ src/components/footer.js
@@ -0,0 +1,13 @@
+import React from 'react'
+
+export const Footer = () => {
+  return (
+    <div>
+      Contact us
+      <ul>
+        <li>email: NotAnEmail@DoNotReply.com</li>
+        <li>phone: (123) 555-4567</li>
+      </ul>
+    </div>
+  )
+}
