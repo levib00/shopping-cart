ed68ff776adde70547811805bc6c4e30d5f4d12c
levib00 <levib8@hotmail.com>
2023-03-09 18:02:29 -0500

    Make component for items in cart

---
+++ src/components/cart-item.js
@@ -0,0 +1,24 @@
+import React from "react";
+import { QuantityInput } from "./quantity-input";
+
+export const CartItem = (props) => {  
+  return (
+    <div>
+      <div>
+        <div>
+          {props.img}
+        </div>
+        <div>
+          {props.name}
+        </div>
+        <QuantityInput thisItem={props.thisItem} cartItems={props.cartItems} setCartItems={props.setCartItems} price={props.price} quantity={props.quantity}/>
+        <div>
+          {(props.price * props.quantity).toFixed(2)}
+        </div>
+        <button>
+          x
+        </button>
+      </div>
+    </div>
+  )
+}
