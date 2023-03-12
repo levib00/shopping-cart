278c3bf1537879cdedad808f35f6e11aefe62b36
levib00 <levib8@hotmail.com>
2023-03-11 20:43:00 -0500

    Adjust tests to follow best practices

--- src/tests/shop-item.test.js
+++ src/tests/shop-item.test.js
@@ -1,9 +1,9 @@
 /* eslint-disable testing-library/no-unnecessary-act */
 import React from "react";
-import { getByRole, render, screen, act } from "@testing-library/react";
+import { render, screen, act } from "@testing-library/react";
 import "@testing-library/jest-dom";
 import userEvent from "@testing-library/user-event";
-import { ShopItem } from '../components/shop-item'
+import { QuantityInput } from '../components/quantity-input'
 import { getValue } from "@testing-library/user-event/dist/utils";
 
 describe("Favorite Input", () => {
@@ -15,88 +15,82 @@ describe("Favorite Input", () => {
 
 describe('Test quantity input', () => {
   
+  let mockItem = [{
+    name: '',
+    price: 1,
+    img: require('../images/access-card.jpg'),
+    quantity:1
+  }]
+
+  const mockSetCart = jest.fn()
+
   it('test increment button', () => {
-    render(<ShopItem />)
+    render(<QuantityInput thisItem={mockItem[0]} cartItems={mockItem} setCartItems={mockSetCart} quantity={mockItem[0].quantity} />)
+    
     const incrementBtn = screen.getByRole('button', {name: '+'})
-    // eslint-disable-next-line testing-library/no-unnecessary-act
-    act(() => {
       for (let i = 0; i < 3; i++) {
-        userEvent.click(incrementBtn)
+        act(() => {
+          userEvent.click(incrementBtn)
+        });
       }
-    });
-    const quantity = screen.getByRole('textbox', {name:'quantity:'}).value
-    expect(quantity).toBe('3')
+    expect(mockSetCart).toBeCalledTimes(3)
     
   })
 
   it('test decrement button', () => {
-    render(<ShopItem />)
-    const incrementBtn = screen.getByRole('button', {name: '+'})
-    act(() => {
+    render(<QuantityInput thisItem={mockItem[0]} cartItems={mockItem} setCartItems={mockSetCart} quantity={mockItem[0].quantity} />)
+
+    const decrementBtn = screen.getByRole('button', {name: '+'})
       for (let i = 0; i < 3; i++) {
-        userEvent.click(incrementBtn)
+        act(() => {
+          userEvent.click(decrementBtn)
+        });
       }
-    });
-    const decrementBtn = screen.getByRole('button', {name: '-'})
-    act(() => {
-      for (let i = 0; i < 3; i++) {
-        userEvent.click(decrementBtn)
-      }
-    });
-    const quantity = screen.getByRole('textbox', {name:'quantity:'}).value
-    expect(quantity).toBe('0')
+    expect(mockSetCart).toBeCalledTimes(3)
   })
   
   it('prevent negative quantities.', () => {
-    render (<ShopItem />)
+    let mockItem = [{
+      name: 'Access Keycard',
+      price: 129.99,
+      img: require('../images/access-card.jpg'),
+      quantity:''
+    }]
+  
+    const mockSetCart = (newArr) => {
+      mockItem = newArr
+    }
+    const {rerender} = render (<QuantityInput thisItem={mockItem[0]} cartItems={mockItem} setCartItems={mockSetCart} quantity={mockItem[0].quantity}/>)
     const decrementBtn = screen.getByRole('button', {name: '-'})
     act(() => {
-      for (let i = 0; i < 3; i++) {
-        userEvent.click(decrementBtn)
-      }
+      userEvent.click(decrementBtn)
     });
-    const quantity = screen.getByRole('textbox', {name:'quantity:'}).value
+    rerender(<QuantityInput thisItem={mockItem[0]} cartItems={mockItem} setCartItems={mockSetCart} quantity={mockItem[0].quantity}/>)
+    const quantity = screen.getByRole('textbox', {name:'Quantity:'}).value
     expect(quantity).toBe('0')
   })
 
   it('prevents quantities more than 25.', () => {
-    render (<ShopItem />)
+    
+    let mockItem = [{
+      name: 'Access Keycard',
+      price: 129.99,
+      img: require('../images/access-card.jpg'),
+      quantity: 25
+    }]
+  
+    const mockSetCart = (newArr) => {
+      mockItem = newArr
+    }
+
+    const {rerender} = render (<QuantityInput thisItem={mockItem[0]} cartItems={mockItem} setCartItems={mockSetCart} quantity={mockItem[0].quantity}/>)
     const incrementBtn = screen.getByRole('button', {name: '+'})
     act(() => {
-      for (let i = 0; i < 30; i++) {
-        userEvent.click(incrementBtn)
-      }
+      userEvent.click(incrementBtn)
     });
-    const quantity = screen.getByRole('textbox', {name:'quantity:'}).value
+    rerender(<QuantityInput thisItem={mockItem[0]} cartItems={mockItem} setCartItems={mockSetCart} quantity={mockItem[0].quantity}/>)
+    const quantity = screen.getByRole('textbox', {name:'Quantity:'}).value
     expect(quantity).toBe('25')
   })
 })
 
-describe("Typed values", () => {
-  it('max quantity works with typed values', () => {
-    render (<ShopItem />)
-    const quantity = screen.getByRole('textbox', {name:'quantity:'})
-    act(() => {
-      userEvent.type(quantity, '30')
-    });
-    expect(quantity.value).toBe('25')
-  });
-
-  it('min quantity works with typed values', () => {
-    render (<ShopItem />)
-    const quantity = screen.getByRole('textbox', {name:'quantity:'})
-    act(() => {
-        userEvent.type(quantity, '-30')
-    });
-    expect(parseInt(quantity.value)).toBeGreaterThan(-1)
-  });
-
-  it('input will not accept non-numbers', () => {
-    render (<ShopItem />)
-    const quantity = screen.getByRole('textbox', {name:'quantity:'})
-    act(() => {
-      userEvent.type(quantity, 'w];,-=+_9')
-    });
-    expect(quantity.value).toBe('9')
-  });
-});
