4cd5aae3558b24609e64a3c132c1406d361ecedf
levib00 <levib8@hotmail.com>
2023-03-17 21:43:10 -0400

    Add classes for styling and add routing

--- src/components/nav-bar.js
+++ src/components/nav-bar.js
@@ -1,18 +1,33 @@
 import React from "react"
 import { Link } from "react-router-dom"
+import wallet from '../assets/images/wallet.svg'
+import '../styles/styles.css'
 
 export const Nav = (props) => {
   return (
-    <header>
-      <span> <img src={require("../assets/images/logo.png")} alt='Terragroup logo'></img>Terragroup Access </span>
-      <ul>
-        <li>
-          <Link to='/'>Home</Link>
+    <header className='header'>
+      <Link className="route-link" to='/'>
+        <span className='hero'>
+          <img className='hero-img' src={require("../assets/images/logo.png")} alt='Terragroup logo'></img> 
+          <div>Terragroup Access</div> 
+        </span>
+      </Link>
+      <ul className='route-link-container'>
+        <li className="route-listing">
+          <Link className="route-link" to='/'><button className="route-button">Home</button></Link>
         </li>
-        <li>
-          <Link to='/shop'>Shop</Link>
+        <li className="route-listing">
+          <Link className="route-link" to='/shop'><button className="route-button">Shop</button></Link>
+        </li>
+        <li className="route-listing">
+        <Link className="route-link" to='/shop'>
+          <button className="route-button wallet-button">
+            <img className='wallet' src={wallet} alt='cart'></img>
+            <div className='circle'>{props.cartQuantity}</div>
+          </button>
+        </Link>
         </li>
       </ul>
-    </header>
+      </header>
   )
 }
