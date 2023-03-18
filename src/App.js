78ded58c1a0b5d9b2a6734f3ad43b81d87c6a6d3
levib00 <levib8@hotmail.com>
2023-03-17 21:47:41 -0400

    Add cart logic and adjust routing

--- src/App.js
+++ src/App.js
@@ -1,18 +1,21 @@
-import React from "react";
+import React, { useState }from "react";
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { Nav } from './components/nav-bar'
 import { Footer } from './components/footer';
 import { Main } from "./components/main";
-import { Shop } from './components/shop'
+import { Shop } from './components/shop';
+import '../src/styles/styles.css'
 
 const App = () => {
+  const [cartQuantity, setCartQuantity] = useState(0)
+  const [cartItems,  setCartItems] = useState([])
   return (
     <div className="App">
       <BrowserRouter>
-        <Nav />
+        <Nav cartQuantity={cartQuantity} />
         <Routes>
           <Route path='/' element={<Main />} />
-          <Route path="/shop" element={<Shop passedCartItems={[]}/>} />
+          <Route path="/shop" element={<Shop setCartQuantity={setCartQuantity} cartItems={cartItems} setCartItems={setCartItems}/>} />
         </Routes>
         <Footer />
       </BrowserRouter>
