6b29fee5918f6f42cbff1d3f4d1ae9597161e1e9
levib00 <levib8@hotmail.com>
2023-03-17 21:42:02 -0400

    Add classes for styling and routing to button

--- src/components/main.js
+++ src/components/main.js
@@ -1,11 +1,15 @@
 import React from "react"
+import { Link } from "react-router-dom"
+import  '../styles/styles.css'
 
 export const Main = () => {
   return (
-    <div> {/*will make this an image bg*/}
-      <h2>this is optional* maybe a tagline</h2>
-      <p>This is some babel about how great our newest product is.</p>
-      <button>SHOP NOW</button>
+    <div className='main'>
+      <div className='text-container'>
+        <h2 className='tagline'>Gain Access To The Hidden Labs</h2>
+        <p className='description'>You wont find colored keycards at a cheaper price anywhere else (unless someone messes up a flea market listing)</p>
+        <Link className="shop-now" to='/shop'><button className='shop-button'>Shop Now</button></Link>
+      </div>
     </div>
   )
 }
